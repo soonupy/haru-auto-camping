208480f5fe2ff1addcb1d02e9d2e79ea83ebd33e
soonupy <soonupy97@naver.com>
2024-12-28 15:36:15 +0900

    start

---
+++ js/common.js
@@ -0,0 +1,33 @@
+// 컴포넌트 로드 함수
+function loadComponent(elementId, url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document
+                .querySelector(elementId)
+                .innerHTML = data;
+        })
+        .catch(error => console.error('컴포넌트 로드 실패:', error));
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    loadComponent('#header-wrap', '../header.html');
+    loadComponent('#footer-wrap', '../footer.html');
+    loadComponent('#sidenav-wrap', '../sideNav.html');
+});
+
+// 카테고리 선택 함수
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.content-wrap');
+    sections.forEach(section => section.style.display = 'none');
+    
+    document.querySelector('.category-1').style.display = 'block';
+    document.querySelector('#category-1').checked = true;
+    
+    document.querySelectorAll('input[name="category"]').forEach(radio => {
+        radio.addEventListener('change', e => {
+            sections.forEach(section => section.style.display = 'none');
+            document.querySelector(`.category-${e.target.id.split('-')[1]}`).style.display = 'block';
+        });
+    });
+});
