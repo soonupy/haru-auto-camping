8fd9a81095ce02900b83887b28b0db9430309ca3
soonupy97 <153884872+soonupy@users.noreply.github.com>
2024-12-31 01:20:18 +0900

    index modify

--- js/common.js
+++ js/common.js
@@ -16,11 +16,21 @@ document.addEventListener('DOMContentLoaded', function() {
         .catch(error => console.error('푸터를 로드하는 중 오류가 발생했습니다:', error));
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    loadComponent('#header-wrap', 'component/header.html');
-    loadComponent('#footer-wrap', 'component/footer.html');
-    loadComponent('#sidenav-wrap', 'component/sideNav.html');
-});
+// loadComponent 함수 정의
+// function loadComponent(targetSelector, url) {
+//     fetch(url)
+//         .then(response => response.text())
+//         .then(data => {
+//             document.querySelector(targetSelector).innerHTML = data;
+//         })
+//         .catch(error => console.error(`${url} 로드 중 오류 발생:`, error));
+// }
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     loadComponent('#header-wrap', 'component/header.html');
+//     loadComponent('#footer-wrap', 'component/footer.html');
+//     loadComponent('#sidenav-wrap', 'component/sideNav.html');
+// });
 
 
 // 카테고리 선택 함수
