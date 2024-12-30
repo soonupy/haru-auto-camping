b4d7610bb9cb84e4c3e5f6dd0af83ea605760b4b
soonupy97 <153884872+soonupy@users.noreply.github.com>
2024-12-30 19:59:00 +0900

    remodify header/footer

--- js/common.js
+++ js/common.js
@@ -1,11 +1,26 @@
-// 컴포넌트 로드 함수
-function loadComponent(selector, url) {
-    fetch(url)
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('component/header.html')
         .then(response => response.text())
         .then(data => {
-            document.querySelector(selector).innerHTML = data;
-        });
-}
+            document.getElementById('header-wrap').innerHTML = data;
+        })
+        .catch(error => console.error('헤더를 로드하는 중 오류가 발생했습니다:', error));
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('component/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer-wrap').innerHTML = data;
+        })
+        .catch(error => console.error('푸터를 로드하는 중 오류가 발생했습니다:', error));
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    loadComponent('#header-wrap', 'component/header.html');
+    loadComponent('#footer-wrap', 'component/footer.html');
+    loadComponent('#sidenav-wrap', 'component/sideNav.html');
+});
 
 
 // 카테고리 선택 함수
--- js/pages.js
+++ js/pages.js
@@ -1,15 +1,6 @@
-// 컴포넌트 로드 함수
-function loadComponent(selector, url) {
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            document.querySelector(selector).innerHTML = data;
-        });
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    loadComponent('#header-wrap', 'component/header.html');
-    loadComponent('#footer-wrap', 'component/footer.html');
-    loadComponent('#sidenav-wrap', 'component/sideNav.html');
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//     loadComponent('#header-wrap', 'component/header.html');
+//     loadComponent('#footer-wrap', 'component/footer.html');
+//     loadComponent('#sidenav-wrap', 'component/sideNav.html');
+// });
 
