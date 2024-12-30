64c078a8d1b75d0fdc1a2a424afda5cdd15f5b04
soonupy97 <153884872+soonupy@users.noreply.github.com>
2024-12-30 19:30:09 +0900

    modify link

--- js/pages.js
+++ js/pages.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    loadComponent('#header-wrap', '../component/header.html');
-    loadComponent('#footer-wrap', '../component/footer.html');
-    loadComponent('#sidenav-wrap', '../component/sideNav.html');
+    loadComponent('#header-wrap', 'component/header.html');
+    loadComponent('#footer-wrap', 'component/footer.html');
+    loadComponent('#sidenav-wrap', 'component/sideNav.html');
 });
 
