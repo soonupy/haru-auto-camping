7f97c6807b979b551a29ca56c3053a7b309e2f84
soonupy97 <153884872+soonupy@users.noreply.github.com>
2024-12-30 19:51:28 +0900

    remodify

--- js/pages.js
+++ js/pages.js
@@ -1,3 +1,12 @@
+// 컴포넌트 로드 함수
+function loadComponent(selector, url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector(selector).innerHTML = data;
+        });
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     loadComponent('#header-wrap', 'component/header.html');
     loadComponent('#footer-wrap', 'component/footer.html');
