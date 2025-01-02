b4fabbd1477a8092923c9fc0205bb493defffe34
soonupy97 <153884872+soonupy@users.noreply.github.com>
2025-01-02 23:00:08 +0900

    add tab feat

--- js/pages.js
+++ js/pages.js
@@ -1,24 +1,24 @@
 // document.addEventListener('DOMContentLoaded', function () {
-//     loadComponent('#header-wrap', 'component/header.html');
-//     loadComponent('#footer-wrap', 'component/footer.html');
-//     loadComponent('#sidenav-wrap', 'component/sideNav.html');
+//     loadComponent('#header-wrap', '../component/header.html');
+//     loadComponent('#footer-wrap', '../component/footer.html');
+//     loadComponent('#sidenav-wrap', '../component/sideNav.html');
 // });
 
 
 
 // 카테고리 선택 함수
-document.addEventListener('DOMContentLoaded', () => {
-    const sections = document.querySelectorAll('.content-wrap');
-    sections.forEach(section => section.style.display = 'none');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const sections = document.querySelectorAll('.content-wrap');
+//     sections.forEach(section => section.style.display = 'none');
     
-    document.querySelector('.category-1').style.display = 'block';
-    document.querySelector('#category-1').checked = true;
+//     document.querySelector('.category-1').style.display = 'block';
+//     document.querySelector('#category-1').checked = true;
     
-    document.querySelectorAll('input[name="category"]').forEach(radio => {
-        radio.addEventListener('change', e => {
-            sections.forEach(section => section.style.display = 'none');
-            document.querySelector(`.category-${e.target.id.split('-')[1]}`).style.display = 'block';
-        });
-    });
-});
+//     document.querySelectorAll('input[name="category"]').forEach(radio => {
+//         radio.addEventListener('change', e => {
+//             sections.forEach(section => section.style.display = 'none');
+//             document.querySelector(`.category-${e.target.id.split('-')[1]}`).style.display = 'block';
+//         });
+//     });
+// });
 
