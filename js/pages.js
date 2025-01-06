96916462c9b6ba2f3b4c196ce2c1a29ac4d8551f
soonupy97 <153884872+soonupy@users.noreply.github.com>
2025-01-06 23:42:55 +0900

    LOCAL TEST

--- js/pages.js
+++ js/pages.js
@@ -1,62 +1,18 @@
-// // document.addEventListener('DOMContentLoaded', function() {
-// //     const categories = [
-// //         { id: 1, name: '매점', enName: 'Mart', link: './mart.html', isComingSoon: false },
-// //         { id: 2, name: '편의시설', enName: 'Amenities', link: './amenities.html', isComingSoon: false },
-// //         { id: 3, name: '산책로/포토존', enName: 'Rest Area', link: './rest.html', isComingSoon: false },
-// //         { id: 6, name: '전기차 충전소', enName: 'EV Charging', link: './charging.html', isComingSoon: false },
-// //         { id: 5, name: '수영장', enName: 'Pool', link: './pool.html', isComingSoon: true },
-// //         { id: 4, name: '카페', enName: 'Cafe', link: './cafe.html', isComingSoon: true }
-// //     ];
+// 전역 변수로 basePath 선언
+const basePath = window.location.origin === 'http://127.0.0.1:5500' 
+    ? 'http://127.0.0.1:5500/' 
+    : 'haru-auto-camping/';
 
-// //     const container = document.getElementById('categoryContainer');
-// //     const currentPath = window.location.pathname;
-
-// //     categories.forEach(category => {
-// //         const input = document.createElement('input');
-// //         input.type = 'radio';
-// //         input.name = 'category';
-// //         input.id = `category-${category.id}`;
-        
-// //         // 현재 페이지 URL에 카테고리의 링크가 포함되어 있으면 체크
-// //         if (currentPath.includes(category.link.split('/').pop())) {
-// //             input.checked = true;
-// //         }
-
-// //         const label = document.createElement('label');
-// //         label.htmlFor = `category-${category.id}`;
-// //         if (category.isComingSoon) {
-// //             label.className = 'comming-soon';
-// //         }
-        
-// //         label.innerHTML = `
-// //             ${category.name}
-// //             <p>${category.enName}</p>
-// //         `;
-
-// //         // 클릭 이벤트 추가
-// //         label.addEventListener('click', () => {
-// //             if (!category.isComingSoon) {
-// //                 location.href = category.link;
-// //             }
-// //         });
-
-// //         container.appendChild(input);
-// //         container.appendChild(label);
-// //     });
-// // });
-
-// $(document).ready(function() {
-//     // 카테고리 컴포넌트 로드
-//     $("#facility-category").load("./facility-category.html", function() {
-//         // 현재 페이지 URL에 따라 active 클래스 추가
-//         const currentPage = window.location.pathname.split('/').pop();
-//         $(`.category-item[href='${currentPage}']`).addClass('active')
-//             .find('input[type="radio"]').prop('checked', true);
-//     });
-// });
 $(document).ready(function() {
-    // 절대 경로로 변경
-    $('.category-list').load('./facility/facility-category.html');
+    $('.category-list').load('./facility/facility-category.html', function() {
+        // HTML이 로드된 후 각 라디오 버튼의 onclick 이벤트 수정
+        $('input[type="radio"]').each(function() {
+            const originalPath = $(this).attr('onclick').match(/location\.href='([^']+)'/)[1];
+            // haru-auto-camping/를 제거하고 basePath를 추가
+            const cleanPath = originalPath.replace('haru-auto-camping/', '');
+            $(this).attr('onclick', `this.checked=true; location.href='${basePath}${cleanPath}'`);
+        });
+    });
 });
 
 window.onload = function() {
