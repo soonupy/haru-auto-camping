e265cbe7fd57ff9e5373acd3a7af882a0f137c1b
soonupy97 <153884872+soonupy@users.noreply.github.com>
2025-01-06 22:04:55 +0900

    add category js

--- js/common.js
+++ js/common.js
@@ -1,24 +1,3 @@
-// $.get('component/header.html', function(data) {
-//     $('#header-wrap').html(data);
-
-//     $('.menu-btn').on('click', function(e) {
-//                     e.preventDefault();
-//                     $(this).toggleClass('active');
-//                     $(this).parent().find('.menu-list').toggleClass('active');
-//                 });
-        
-//                 $('.menu-category').on('click', function(e) {
-//                     e.preventDefault();
-        
-//                     $('.menu-category').not(this).siblings('.menu-list-2depth').removeClass('active').parent().find('.accordion-arrow').removeClass('active');
-//                     $(this).find('.accordion-arrow').toggleClass('active');
-//                     $(this).parent().find('.menu-list-2depth').toggleClass('active');
-//                 });
-// });
-
-// $.get('component/footer.html', function(data) {
-//     $('#footer-wrap').html(data);
-// });
 
 $(function() {
     // GitHub Pages 여부 확인
@@ -66,3 +45,5 @@ $(function() {
         $('#footer-wrap').html(data);
     });
 });
+
+
--- js/pages.js
+++ js/pages.js
@@ -1,38 +1,46 @@
-$(function() {
-    // 현재 페이지의 경로를 확인하여 컴포넌트 경로 설정
+document.addEventListener('DOMContentLoaded', function() {
+    const categories = [
+        { id: 1, name: '매점', enName: 'Mart', link: '../facility/mart.html', isComingSoon: false },
+        { id: 2, name: '편의시설', enName: 'Amenities', link: '../facility/amenities.html', isComingSoon: false },
+        { id: 3, name: '산책로/포토존', enName: 'Rest Area', link: '../facility/rest.html', isComingSoon: false },
+        { id: 6, name: '전기차 충전소', enName: 'EV Charging', link: '../facility/charging.html', isComingSoon: false },
+        { id: 5, name: '수영장', enName: 'Pool', link: '../facility/pool.html', isComingSoon: true },
+        { id: 4, name: '카페', enName: 'Cafe', link: '../facility/cafe.html', isComingSoon: true }
+    ];
+
+    const container = document.getElementById('categoryContainer');
     const currentPath = window.location.pathname;
-    const isSubPage = currentPath.split('/').length > 2;
-    const componentPath = isSubPage ? '../component/' : './component/';
 
-    // 헤더 로드
-    $.get(componentPath + 'header.html', function(data) {
-        $('#header-wrap').html(data);
-
-        // 메뉴 버튼 클릭 이벤트
-        $('.menu-btn').on('click', function(e) {
-            e.preventDefault();
-            $(this).toggleClass('active');
-            $(this).parent().find('.menu-list').toggleClass('active');
-        });
+    categories.forEach(category => {
+        const input = document.createElement('input');
+        input.type = 'radio';
+        input.name = 'category';
+        input.id = `category-${category.id}`;
         
-        // 메뉴 카테고리 클릭 이벤트
-        $('.menu-category').on('click', function(e) {
-            e.preventDefault();
-            $('.menu-category').not(this).siblings('.menu-list-2depth').removeClass('active')
-                .parent().find('.accordion-arrow').removeClass('active');
-            $(this).find('.accordion-arrow').toggleClass('active');
-            $(this).parent().find('.menu-list-2depth').toggleClass('active');
-        });
-    });
+        // 현재 페이지 URL에 카테고리의 링크가 포함되어 있으면 체크
+        if (currentPath.includes(category.link.split('/').pop())) {
+            input.checked = true;
+        }
 
-    // 푸터 로드
-    $.get(componentPath + 'footer.html', function(data) {
-        $('#footer-wrap').html(data);
+        const label = document.createElement('label');
+        label.htmlFor = `category-${category.id}`;
+        if (category.isComingSoon) {
+            label.className = 'comming-soon';
+        }
+        
+        label.innerHTML = `
+            ${category.name}
+            <p>${category.enName}</p>
+        `;
+
+        // 클릭 이벤트 추가
+        label.addEventListener('click', () => {
+            if (!category.isComingSoon) {
+                location.href = category.link;
+            }
+        });
+
+        container.appendChild(input);
+        container.appendChild(label);
     });
 });
-
-
-
-
-
-
